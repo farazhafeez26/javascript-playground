d6e6ef309b63385a7d6a05b48e5a87a161b9b4e1
farazhafeez <faraz.hafiz@gmail.com>
2020-08-28 20:48:46 +0200

    played with JS

--- askedstuff.js
+++ askedstuff.js
@@ -4,3 +4,31 @@ if (readDocs) {
 } else {
   alert("you will be a good programmer");
 }
+
+<script>
+
+  var name = "Faraz";
+  var fullName = name + " " + "hafeez";
+  var age = 33;
+  var isCoding = true;
+  var hobbies = ["Surfing internet", "Cooking", "coding"]
+var programmer = {
+    hobbies,
+    name: "faraz hafeez",
+  age: age,
+  sayhi: function () {
+    console.log(this.name);
+    return this.age
+  }
+
+};
+programmer.sayhi()
+function createPar(text) {
+  var p = document.createElement("P")
+  p.innerText = text
+  document.body.appendChild(p)
+}
+console.log(hobbies.length)
+for (var i = 0; i < hobbies.length; {
+    createPar(hobbies[i])
+  }
